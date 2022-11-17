f74af0a4f76da0f66fb505557819937475aa7291
EzeSosa <ezequielsosa423@gmail.com>
2022-11-16 22:12:12 -0300

    update

--- consult/interfaz_equipo.py
+++ consult/interfaz_equipo.py
@@ -7,19 +7,19 @@ def crear_interfaz(app, obj): # creando la nueva interfaz para los equipos
                                               labelanchor = 'n').pack (padx = 10, pady = 10)
     
     lblNombre = lambda: tk.Label (form_datosEquipo, text = f" Nombre del equipo: {obj['nombreEquipo']}", # nombre del equipo
-                          font = ('Arial', 12)).place (x = 20, y = 40)
+                          font = ('Arial', 12)).place (x = 20, y = 41)
 
     lblId = lambda: tk.Label (form_datosEquipo, text = f" ID del equipo: {obj['idEquipo']}", # ID del equipo
-                          font = ('Arial', 12)).place (x = 20, y = 60)
+                          font = ('Arial', 12)).place (x = 20, y = 61)
     
     lblDireccion = lambda: tk.Label (form_datosEquipo, text = f" Dirección del equipo: {obj['direccionEquipo']}", # dirección del equipo
-                          font = ('Arial', 12)).place (x = 20, y = 80)
+                          font = ('Arial', 12)).place (x = 20, y = 81)
     
     lblLocalidad = lambda: tk.Label (form_datosEquipo, text = f" Nombre del equipo: {obj['localidadEquipo']}", # localidad del equipo
-                          font = ('Arial', 12)).place (x = 20, y = 100)
+                          font = ('Arial', 12)).place (x = 20, y = 101)
     
     lblNombreRespo = lambda: tk.Label (form_datosEquipo, text = f" Nombre del responsable: {obj['nombreResponsable']}", # responsable del equipo
-                          font = ('Arial', 12)).place (x = 20, y = 120)
+                          font = ('Arial', 12)).place (x = 20, y = 121)
 
     lblFrDatosEquipo(), lblNombre(), lblId(),
     lblDireccion(), lblLocalidad(), lblNombreRespo() # llamando las funciones
--- interfaz.py
+++ interfaz.py
@@ -3,6 +3,8 @@ from registers import registrar_equipo
 from consult import consultar_equipo
 
 app = tk.Tk() # creando la ventana
+app.title('Sistema Principal') # título de la ventana
+
 config = lambda: app.config(height = 150, width = 370) # config de la ventana
 
 labelfr = lambda: tk.LabelFrame (app, text = " Federación de Hockey ", width = 370, height = 150, # creando el labelframe
@@ -10,11 +12,11 @@ labelfr = lambda: tk.LabelFrame (app, text = " Federación de Hockey ", width = 
 
 regis_equipo = lambda: tk.Button (app, text = 'Registrar Equipo', bg = "#77DD77", # creando el boton para registrar multas
                                   command = lambda: registrar_equipo.crear_interfaz(app), # llamando la función para la nueva ventana
-                                  width = 15, font = ('Arial', 12)).place (x = 40, y = 100)
+                                  width = 15, font = ('Arial', 12)).place (x = 40, y = 95)
 
 regis_multa = lambda: tk.Button(app, text = 'Consultar Equipo', bg = "#77DD77", # creando el boton para registrar multas
                                 command = lambda: consultar_equipo.crear_interfaz(app), # llamando a la interfaz
-                                width = 15, font = ('Arial', 12)).place (x = 200, y = 100)
+                                width = 15, font = ('Arial', 12)).place (x = 200, y = 95)
 
 config(), labelfr(), regis_equipo(), regis_multa() # llamando a las funciones
 app.mainloop() # para cargar la ventana
